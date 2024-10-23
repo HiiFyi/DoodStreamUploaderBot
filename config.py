d0f92cb3d2009ebab1a27bdda242ddbbe3d417e5
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2024-10-23 10:32:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from os import environ
 
 ### Getting ENvironment Variables
 class Config(object):
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") # Make a bot from https://t.me/BotFather
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7609207287:AAHNsu8KgFmicSN0aPIRcJe0DaeVeluV3hk") # Make a bot from https://t.me/BotFather
     
-    APP_ID = int(environ.get("API_ID", "0")) # Get this value from https://my.telegram.org/apps
+    APP_ID = int(environ.get("API_ID", "24935727")) # Get this value from https://my.telegram.org/apps
     
-    API_HASH = environ.get("API_HASH", "") # Get this value from https://my.telegram.org/apps
+    API_HASH = environ.get("API_HASH", "3fd33336629324ecd664e9b6894f0909") # Get this value from https://my.telegram.org/apps
     
-    OWNER_ID = int(environ.get("OWNER_ID", None)) # Your(owner's) telegram id
+    OWNER_ID = int(environ.get("OWNER_ID", "7336971189")) # Your(owner's) telegram id
     
-    MONGO_STR = environ.get("MONGO_STR", "") # Get from MongoDB Atlas
+    MONGO_STR = environ.get("MONGO_STR", "mongodb+srv://sujay5372222222:sujay5372222222@cluster0000007.fdufm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0000007") # Get from MongoDB Atlas
 
